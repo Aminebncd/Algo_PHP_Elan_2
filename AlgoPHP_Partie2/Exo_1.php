fb9bbe34bdab49984f2627b8fee41cb9963bd649
aminebncd <amine.bounachada@hotmail.com>
2024-01-03 14:45:20 +0100

    commit initial

---
+++ AlgoPHP_Partie2/Exo_1.php
@@ -0,0 +1,13 @@
+
+<h1>I. Exercice 1</h1>
+
+
+<p>Créer une fonction personnalisée convertirMajRouge() permettant de transformer une chaîne de
+caractère passée en argument en majuscules et en rouge.
+Vous devrez appeler la fonction comme suit : convertirMajRouge($texte) ;
+</p>
+
+<?php
+
+$texte = « Mon texte en paramètre »; 
+
